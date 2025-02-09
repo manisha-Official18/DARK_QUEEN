426d4af4404f920f05cb5514d1d8a80e087af4b8
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-02-09 17:41:43 +0530

    💗ᴅᴀʀᴋ_Qᴜᴇᴇɴ💮

--- plugins/restart.js
+++ plugins/restart.js
@@ -10,23 +10,28 @@ whatsapp number:94721551183
 */
 
 
+
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const {sleep} = require('../lib/functions')
+
 cmd({
-    pattern: "alive",
-    react: "👋",
-    desc: "Check bot online or no.",
-    category: "main",
+    pattern: "restart",
+    alias: ["rebot","reboot"], 
+    react: "🔃",
+    desc: "restart the bot",
+    category: "owner",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+if (!isOwner) return;
+const {exec} = require("child_process")
+reply("restarting...")
+await sleep(1500)
+exec("pm2 restart all")
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
-//========================
-
