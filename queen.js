5f4edd5e5e9e66dfc3bd25893bd191cdb69fe745
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-02-11 21:27:00 +0530

    💗ᴅᴀʀᴋ_Qᴜᴇᴇɴ💮

--- queen.js
+++ queen.js
@@ -76,7 +76,7 @@ async function connectToWA() {
 
             let up = config.START_MSG;
                     
-            conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/zvx007.jpg` }, caption: up })
+            conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/gdq9qa.jpg` }, caption: up })
 
     //--------------------| DARK_QUEEN  Settings Input |--------------------//
 
