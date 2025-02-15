31188d3e7a472347c72a6522bd47f2fcabc7c3b2
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-02-15 23:53:54 +0530

    💗ᴅᴀʀᴋ_Qᴜᴇᴇɴ💮

--- config.js
+++ config.js
@@ -5,30 +5,19 @@ module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
   SESSION_ID: process.env.SESSION_ID || "",
   // ADD Your Session Id 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "",
   PREFIX: process.env.PREFIX || ".",
   // Add Your Custom Prefix 
-  SENDE_NUMBER_REACT: process.env.SENDE_NUMBER_REACT ||"947xxxxxx",
+  OWNER_NUMBER_REACT: process.env.OWNER_NUMBER_REACT ||"",
   //add number reacat
   mode: process.env.mode || "public",
   // Add Your Bot Mode 
   // private = Only Working For Owner Number
   // public = AnyOne Working
   // inbox = Only Working  Inbox
-  // groups = only working in group
-  //========================================- OTHER - CONFIGS -=====================================================================
-  ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
-  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-  ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "pakaya,hutta,kariya,ponnaya").split(','),
-  ANTI_LINK: process.env.ANTILINK || "true",
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-  AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
-  ALWAYS_TYPING: process.env.ALWAYS_TYPING || "true",
-  ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
-  ANTI_BOT: process.env.ANTI_BOT || "true",
-  ANTI_DELETE: process.env.ANTI_DELETE || "true",
+  // groups = only working in group  
 //=============API KEY=============
   MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5",
   GEMINI_API_KEY: process.GEMINI_API_KEY ||"AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc",
   //==============START================
-  START_MSG: process.env.START_MSG || `╭──────────────⊷❒\n\n*🩷WELCOME TO DARK_QUEEN WHATSAPP BOT.*\n\n┗━┫*⚬°❀𝐌❀°⚬*┣━⁠⁠✾\n\n*🩷DARK_QUEEN is a fast growing Whatsapp bot.*\n\n┗━┫*⚬°❀𝐀❀°⚬*┣━⁠⁠✾\n\n *🩷Your account is now active.*\n\n┗━┫*⚬°❀𝐍❀°⚬*┣━⁠⁠✾\n\n⁠*🩷This is your log number. Avoid using commands here⚠️.*\n\n┗━┫*⚬°❀𝐈❀°⚬*┣━⁠⁠⁠⁠✾\n\n*🩷Join My WhatsApp Channel:* https://whatsapp.com/channel/0029Vb1AB158F2pCzMyd8O1B\n\n┗━┫*⚬°❀𝐘❀°⚬*┣━⁠⁠⁠⁠✾\n\n*🩷CREATED BY MANISHA SASMITHA.*\n\n┗━┫*⚬°❀𝐀❀°⚬*┣━⁠⁠⁠⁠✾\n\n╰──────────────⊷❒` ,
 };
