7f071e6087e3c2389b49dacc98d7e9d3e0b12e4c
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-02-16 00:29:58 +0530

    💗ᴅᴀʀᴋ_Qᴜᴇᴇɴ💮

--- config.js
+++ config.js
@@ -3,20 +3,14 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "",
-  // ADD Your Session Id 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "",
- //add owner number
-  PREFIX: process.env.PREFIX || ".",
-  // Add Your Custom Prefix
-  mode: process.env.mode || "public",
-  // Add Your Bot Mode 
-  // private = Only Working For Owner Number
-  // public = AnyOne Working
-  // inbox = Only Working  Inbox
-  // groups = only working in group  
+  SESSION_ID: process.env.SESSION_ID || "", // ADD Your Session Id 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "", //add owner number
+  OWNER_REACT: process.env.OWNER_REACT || "", // add you owner number
+  OWNER_REACT_EMOJI: process.env.OWNER_REACT_EMOJI || "🙂", //add you owner emoji
+  PREFIX: process.env.PREFIX || ".", // Add Your Custom Prefix
+  mode: process.env.mode || "private",// Add Your Bot Mode 👇
 //=============API KEY=============
-  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5",
-  GEMINI_API_KEY: process.GEMINI_API_KEY ||"AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc",
-  //==============START================
+  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5", // movie api key
+  GEMINI_API_KEY: process.GEMINI_API_KEY ||"AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc", // gemini api key 
+  //=============================================================
 };
