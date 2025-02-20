fd94953c3f2918311390878696efc2f3baf44484
manisha Sasmitha <manisha1718manisha@gmail.com>
2025-02-20 16:50:33 +0530

    💗ᴅᴀʀᴋ_Qᴜᴇᴇɴ💮

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
   SESSION_ID: process.env.SESSION_ID || "", // ADD Your Session Id 
   OWNER_NUMBER: process.env.OWNER_NUMBER || "", //add owner number
-  PREFIX: process.env.PREFIX: || ".", // Add Your Custom Prefix 
-  MODE: process.env.MODE: || "inbox",  // Add Your Bot Mode 
+  PREFIX: process.env.PREFIX || ".", // Add Your Custom Prefix 
+  MODE: process.env.MODE || "inbox",  // Add Your Bot Mode 
  //==================================================API KEY======================================================================
   MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5", // https://api.skymansion.site/movies-dl
   GEMINI_API_KEY: process.GEMINI_API_KEY ||"AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc", // https://aistudio.google.com/prompts/new_chat?gad_source=1&gclid=CjwKCAiAh6y9BhBREiwApBLHC50TkPdPNhXo-Fp32k3EdtcZTb3xlpTFT6sBsRr64eaajRHntsvNwhoCjk8QAvD_BwE
